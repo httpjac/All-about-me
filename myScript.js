e0d0e3cab10da8018706406c18455fb781cf75ff
Jacquie <jacquian97@yahoo.com>
2019-07-17 10:32:51 -0500

    first commit

---
+++ myScript.js
@@ -0,0 +1,7 @@
+function showtime() {
+document.getElementById('demo').innerHTML = Data();
+};
+
+function facts() {
+document.getElementById("demo").innerHTML = "Sloths are clumsy on land but are great swimmers."
+}
